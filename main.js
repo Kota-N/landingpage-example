8d7993d851087e66872aaed42ccc194909a33d2a
Kota <kotancell@gmail.com>
2021-01-18 17:19:58 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// $('.navbar a').on('click', (e) => {
+//   const hash = e.target.hash;
+
+//   $('html, body').animate(
+//     {
+//       scrollTop: $(hash).offset().top,
+//     },
+//     800
+//   );
+// });
